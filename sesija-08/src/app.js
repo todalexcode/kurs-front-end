01014b326a19862a548e1e747b79a15b85a52e10
Radovan Ostojic <Radovan.Ostojic@Vanderlande.com>
2018-02-23 17:09:34 +0100

    Added small game for session 08

---
+++ sesija-08/src/app.js
@@ -0,0 +1,27 @@
+function printNumber(number) {
+    
+    //1. Uzimam element iz HTML
+    //var square = document.getElementById("square");
+
+    //2. Setujem innerHTML property
+    //square.innerHTML = number;
+  }
+  
+  //dodeljujem promenljivoj vrednost objekta 
+  var button = document.getElementById('button');
+  
+  //definsem onclick
+  button.onclick = function() {
+    var result = dice.roll();
+    printNumber(result);
+  };
+  
+
+  //definisem objekat dice
+  var dice = {
+    sides: 6,
+    roll: function() {
+     var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+      return randomNumber;
+    }
+  }
