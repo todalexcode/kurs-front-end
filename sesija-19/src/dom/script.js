ac53238b21b78e50135e4cc03b1db9cce55852b0
Radovan Ostojic <Radovan.Ostojic@Vanderlande.com>
2018-03-23 20:49:34 +0100

    posle sesije 19

--- sesija-19/src/dom/script.js
+++ sesija-19/src/dom/script.js
@@ -1,9 +1,31 @@
-var text = 'dobro dosli u code.edu.rs';
+
+//Sa greskom
+// var text = 'dobro dosli u code.edu.rs';
+// var div = document.getElementById('rollercoaster');
+// var fontSize = 10;
+// for (var i = 0; i < text.length; i++) {
+//   var span = document.createElement('span');
+//   fontSize += 5;
+//   span.style.fontSize = fontSize + 'px';
+//   span.textContent = text[i];
+//   div.appendChild(span);
+// }
+
+
+//tacan
+var text = "anavolimilovana";
 var div = document.getElementById('rollercoaster');
-var fontSize = 10;
-for (var i = 0; i < text.length; i++) {
+var fontSize = 5;
+//console.log(text.length);
+for(var i = 0; i < text.length; i++){
   var span = document.createElement('span');
-  fontSize += 5;
+  if(i < text.length/2){
+     fontSize += 5;
+   }
+  else if(i > text.length/2){
+     fontSize -= 5;
+   }
+
   span.style.fontSize = fontSize + 'px';
   span.textContent = text[i];
   div.appendChild(span);
---
+++ sesija-19/src/objects-cc/script.js
@@ -0,0 +1,49 @@
+//Code challenge 1
+
+var recipe = {
+    'title': 'Mole',
+    'servings': 2,
+    'ingredients': ['cumin', 'cinnamon', 'cocoa']
+};
+
+console.log(recipe.title);
+console.log('Servings: ' + recipe.servings);
+console.log('Ingredients:');
+for (var i = 0; i < recipe.ingredients.length; i++) {
+    console.log(recipe.ingredients[i]);
+}
+
+
+//Code challenge 2
+var books = [
+    {title: 'The Design of EveryDay Things',
+     author: 'Don Norman',
+     alreadyRead: false
+    },
+    {title: 'The Most Human Human',
+    author: 'Brian Christian',
+    alreadyRead: true
+    }];
+  
+  for (var i = 0; i < books.length; i++) {
+    var book = books[i];
+    var bookInfo = book.title + '" by ' + book.author;
+    if (book.alreadyRead) {
+      console.log('You already read "' + bookInfo);
+    } else {
+      console.log('You still need to read "' + bookInfo);
+    }
+  }
+
+  //code challenge 3
+  var myFavMovie = {
+    title: 'Puff the Magic Dragon',
+    duration: 30,
+    stars: ['Puff', 'Jackie', 'Living Sneezes']
+};
+
+function printMovie(movie) {
+    console.log(movie.title + ' lasts for ' + movie.duration + ' minutes');
+    console.log('It stars ' + movie.stars.join(', '));
+}
+printMovie(myFavMovie);
