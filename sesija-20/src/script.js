c3f309c80e96782026c5d219670b6762e0e41822
CT\sinisav <sinisa.vrhovac@comtrade.com>
2018-03-26 18:43:38 +0200

    Vjezbe

--- sesija-20/src/script.js
+++ sesija-20/src/script.js
@@ -1,5 +1,4 @@
 console.log("Hello, world!");
-console.log(2+8);
-console.log(2+2*2);
-console.log("2" + 2);
-//alert("Zdravo svete");
+// console.log(2+8);
+// console.log(2+2*2);
+// console.log("2" + 2);
