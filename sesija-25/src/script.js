f456cebd6b41cd20a123f95222a7f619e1c6850e
Radovan Ostojic <Radovan.Ostojic@Vanderlande.com>
2018-04-11 12:22:34 +0200

    pre sesije 25

--- sesija-25/src/script.js
+++ sesija-25/src/script.js
@@ -1,5 +1,26 @@
-console.log("Hello, world!");
-console.log(2+8);
-console.log(2+2*2);
-console.log("2" + 2);
-//alert("Zdravo svete");
+var header = document.querySelector('header');
+var section = document.querySelector('section');
+
+
+function populateHeader(jsonObj) {
+ 
+}
+
+function showHeroes(jsonObj) {
+  var heroes = jsonObj['members'];
+  for(var i = 0; i < heroes.length; i++) {
+ 
+    myH2.textContent = heroes[i].name;
+ 
+    var superPowers = heroes[i].powers;
+    for(var j = 0; j < superPowers.length; j++) {
+   
+    }
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myList);
+    section.appendChild(myArticle);
+  }
+}
