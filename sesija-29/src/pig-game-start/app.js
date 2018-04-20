8a883b4e95b965a84ec5908bcdbc0e2f8d05892a
Radovan Ostojic <Radovan.Ostojic@Vanderlande.com>
2018-04-20 20:55:02 +0200

    posle sesije 29

--- sesija-29/src/pig-game-start/app.js
+++ sesija-29/src/pig-game-start/app.js
@@ -8,3 +8,97 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, gamePlaying;
+
+function init(){
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+    
+    document.querySelector('.dice').style.display = 'none';
+    
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    
+    document.querySelector('.player-0-panel').classList.add('active');
+}
+
+init();
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    if(gamePlaying){
+        var dice = Math.floor(Math.random() * 6) + 1;
+        console.log(dice);
+    
+        //dispaly result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+    
+        //update round score IF the rolled number was NOT a 1
+        if (dice !== 1) {
+            //add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        }
+        else {
+            nextPlayer();
+        }
+    }
+ });
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    // if(activePlayer === 0){
+    //     activePlayer = 1;
+    // }
+    // else{
+    //     activePlayer = 0;
+    // }
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    if(gamePlaying){
+        //add current score to GLOBAL scores
+        scores[activePlayer] += roundScore;
+
+        //update UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        //check if the active player won the game
+        if(scores[activePlayer] >= 100){
+            document.querySelector('#name-'+ activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        }
+        else{
+            nextPlayer();
+        }
+    }
+});
+
